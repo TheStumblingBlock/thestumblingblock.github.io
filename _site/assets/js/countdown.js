e7cc336a0f9a0f646b75b793cd302a9ec37dbde8
TheStumblingBlock <56649184+TheStumblingBlock@users.noreply.github.com>
2019-10-19 01:47:59 +0300

    Update countdown.js

--- _site/assets/js/countdown.js
+++ _site/assets/js/countdown.js
@@ -1,13 +1,46 @@
+var xmlHttp;
+function srvTime(){
+    try {
+        //FF, Opera, Safari, Chrome
+        xmlHttp = new XMLHttpRequest();
+    }
+    catch (err1) {
+        //IE
+        try {
+            xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+        }
+        catch (err2) {
+            try {
+                xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+            catch (eerr3) {
+                //AJAX not supported, use CPU time.
+                alert("AJAX not supported");
+            }
+        }
+    }
+    xmlHttp.open('HEAD',window.location.href.toString(),false);
+    xmlHttp.setRequestHeader("Content-Type", "text/html");
+    xmlHttp.send('');
+    return xmlHttp.getResponseHeader("Date");
+}
+
+var st = srvTime();
+var date = new Date(st);
+
+
 var leadZero = function(n) {
     if (n < 10 && n >= 0)
         return '0' + n;
     else
 		return n;
 }
+var now = new Date(st);
+
 var countDownDate = new Date("Nov 19, 2019 00:00:00").getTime();
 var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
+  now -= -1000;
+  var distance = countDownDate - now + new Date(st).getTimezoneOffset() * 60;
   var hours = leadZero(Math.floor(distance / (1000 * 60 * 60)));
   var minutes = leadZero(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
   var seconds = leadZero(Math.floor((distance % (1000 * 60)) / 1000));
@@ -20,9 +53,3 @@ var x = setInterval(function() {
     document.getElementById("countdown").innerHTML = '';
   }
 }, 1000);
-
-function getServerTime() {
-  return $.ajax({async: false}).getResponseHeader( 'Date' );
-}
-console.log('Server Time: ', getServerTime());
-console.log('Locale Time: ', new Date(getServerTime()));
