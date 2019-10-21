0ab37cf55ba6e176fd270a615feefad4bd12584a
TheStumblingBlock <56649184+TheStumblingBlock@users.noreply.github.com>
2019-10-22 00:09:01 +0300

    Update

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,3 +1,25 @@
+/* --- Parallax background --- */
+let dde = document.documentElement;
+let value = 55, mx = 0, my = 0, lastposx = 0, lastposy = 0;
+dde.addEventListener("mousemove", e => {
+    let ow = dde.offsetWidth; 
+    let oh = dde.offsetHeight;
+    lastposx = e.clientX * value / ow;
+    lastposy = e.clientY * value / oh; 
+});
+
+window.requestAnimationFrame(render);
+function render (){
+    mx = lerp(mx, lastposx, 0.02);
+    my = lerp(my, lastposy, 0.02);
+    dde.style.setProperty('--mouseX', mx + "%");
+    dde.style.setProperty('--mouseY', my + "%");
+    window.requestAnimationFrame(render);
+}
+function lerp(a, b, n) {
+    return (1 - n) * a + n * b;
+}
+/* --- Countdown --- */
 var xmlHttp;
 function srvTime(){
     try {
@@ -53,5 +75,12 @@ var x = setInterval(function() {
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = '';
+    showContent();
   }
 }, 1000);
+
+function showContent() {
+    var temp = document.getElementsByTagName("template")[0];
+    var clon = temp.content.cloneNode(true);
+    document.getElementById("countdown").appendChild(clon);
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
+/* --- Parallax background --- */
+let dde = document.documentElement;
+let value = 55, mx = 0, my = 0, lastposx = 0, lastposy = 0;
+dde.addEventListener("mousemove", e => {
+    let ow = dde.offsetWidth; 
+    let oh = dde.offsetHeight;
+    lastposx = e.clientX * value / ow;
+    lastposy = e.clientY * value / oh; 
+});
+
+window.requestAnimationFrame(render);
+function render (){
+    mx = lerp(mx, lastposx, 0.02);
+    my = lerp(my, lastposy, 0.02);
+    dde.style.setProperty('--mouseX', mx + "%");
+    dde.style.setProperty('--mouseY', my + "%");
+    window.requestAnimationFrame(render);
+}
+function lerp(a, b, n) {
+    return (1 - n) * a + n * b;
+}
+/* --- Countdown --- */
 var xmlHttp;
 function srvTime(){
     try {
@@ -40,9 +62,8 @@ var now = new Date(st);
 //kak zhe ono nas zaebalo
 //js dlya pedikov
 var x = setInterval(function() {
-  var countDownDate = new Date(st);
   now -= -1000;
-  countDownDate -= -new Date("Nov 19, 2019 00:00:00").getTime()/1000;
+  countDownDate = new Date("Nov 19, 2019 00:00:00 GMT+0300").getTime();
   var distance = countDownDate - now;
   var hours = leadZero(Math.floor(distance / (1000 * 60 * 60)));
   var minutes = leadZero(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
@@ -54,5 +75,12 @@ var x = setInterval(function() {
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = '';
+    showContent();
   }
 }, 1000);
+
+function showContent() {
+    var temp = document.getElementsByTagName("template")[0];
+    var clon = temp.content.cloneNode(true);
+    document.getElementById("countdown").appendChild(clon);
+}
